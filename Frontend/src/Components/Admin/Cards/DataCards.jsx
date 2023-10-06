f055fdddaab1e68ef71e7d9f061a00693185d721
jithin <jithinjolly15@gmail.com>
2023-10-06 19:09:41 +0530

    rider dashboard cards

---
+++ Frontend/src/Components/Admin/Cards/DataCards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DataCards = (props) => {
+  return (
+    <div className={`${props.bgColor} m-1 p-10 w-96 border rounded-xl`}>
+      <p className='text-3xl pb-3'>{props.title}</p>
+      <p className='pb-3'>{props.text}</p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Claim Voucher
+      </button>
+
+    </div>
+  )
+}
+
+export default DataCards
--- Frontend/src/Pages/Rider/RiderDashBoard.jsx
+++ Frontend/src/Pages/Rider/RiderDashBoard.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from '../../Components/Rider/RiderHeader'
+import DataCards from '../../Components/Admin/Cards/DataCards'
 
 const RiderDashBoard = () => {
   return (
     <>
       <Header/>
-      <h1 className='mt-10 text-5xl'>Rider DashBoard</h1>
+      
+      <div className='flex justify-around mt-10'>
+      <DataCards title={'50'} text={"total delivery's complete today"} bgColor={'bg-red-400'}/>
+      <DataCards title={'1000₹'} text={"total earnings Today"} bgColor={'bg-yellow-500'}/>
+      </div>
+
+      <div className='flex justify-around mt-10'>
+      <DataCards title={'5'} text={"total Hour’s worked today"} bgColor={'bg-blue-400'}/>
+      <DataCards title={'2000₹'} text={"Wallet"} bgColor={'bg-lime-500'}/>
+      </div>
     </>
   )
 }
